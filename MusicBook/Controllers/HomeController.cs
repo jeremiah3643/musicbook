76e84808adb5d79c44712f804f6dbe5ab5c094f5
Jeremiah <jeremiah3643@gmail.com>
2019-01-04 14:21:08 -0600

    authorize added and and instruments show up on home

--- MusicBook/Controllers/HomeController.cs
+++ MusicBook/Controllers/HomeController.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MusicBook.Data;
 using MusicBook.Models;
+using MusicBook.Models.ViewModels;
 
 namespace MusicBook.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager< ApplicationUser > signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.ApplicationUsers.ToListAsync());
+            var user = await _userManager.GetUserAsync(User);
+            List<Instrument> AllInstruments = _context.Instruments.ToList();
+           
+            var UserInstruments = _context.PlayerInstruments.Include(pi => pi.Instrument).Where(inst => inst.ApplicationUserId == user.Id).ToList();
+            List<Instrument> userInstrumentList = UserInstruments.Select(inst => inst.Instrument).ToList();
+            
+            HomeViewModel currentHomeInfo = new HomeViewModel();
+            currentHomeInfo.CurrentUser = user;
+            currentHomeInfo.UserInstruments = userInstrumentList;
+        
+            
+            return View(currentHomeInfo);
         }
 
         public IActionResult About()
---
+++ MusicBook/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicBook.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        // need all current user info and list of instruments
+       public List<Instrument> UserInstruments { get; set; }
+       public ApplicationUser CurrentUser { get; set; }
+        
+
+
+    }
+}
